a8ef34831aff65152d5b7d26f0d50fddf6a2c408
Giusi Balsamo <giusi.balsamo@avanade.com>
2020-12-01 10:38:11 +0100

    Add project files.

---
+++ Ticketing.Client/Context/TicketContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Ticketing.Client.Model;
+using Ticketing.Helpers;
+
+namespace Ticketing.Client.Context
+{
+    public class TicketContext: DbContext
+    {
+        DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            string connString= Config.GetConnectionString("TicketDb");
+            //oppure
+            //string connString = config.GetSection("ConnectionStrings")["TicketDb"];
+
+            optionBuilder.UseSqlServer(connString);
+        }
+    }
+}
---
+++ Ticketing.Helpers/Config.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Ticketing.Helpers
+{
+    public class Config
+    {
+        private static IConfigurationRoot config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build(); //forzo il configbuilder ad andare a cercare il programma nella currentdirectory
+
+        public static string GetConnectionString(string connStringName)
+        {
+             return config.GetConnectionString(connStringName);
+             //oppure
+             //string connString = config.GetSection("ConnectionStrings")["TicketDb"];
+        }
+
+        public static IConfigurationSection GetSection(string sectionName)
+        {
+            return config.GetSection(sectionName);
+        }
+       
+    }
+}
